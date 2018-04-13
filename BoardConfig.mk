omnirom/android_device_motorola_athene

athene: Update for early-mounted system partition
 * This removes /system entry from fstab, since this partition
   is now early-mounted and the fstab entry is specified in device tree.

 * At the same time, also include a fully populated fstab, in order to avoid
   build breakage while generating recovery updater scripts.

Change-Id: I8e558458b984e97a45a920c3751a6b7c3f2ebc5c

BoardConfig.mk
@@ -178,7 +178,7 @@ USE_SENSOR_MULTI_HAL := true
 
 # Recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/rootdir/etc/recovery.fstab
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 

ResurrectionRemix-Devices/device_motorola_athene

BoardConfig.mk
@@ -177,7 +177,7 @@ USE_SENSOR_MULTI_HAL := true
 
 # Recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/recovery.fstab
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 